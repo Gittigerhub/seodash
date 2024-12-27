c8c211f60cc89abdd7fb33764c4147cc3895d904
muziktiger <jh.24workspace@gmail.com>
2024-12-27 12:38:18 +0900

    seodash templates 연습 완료

---
+++ src/main/java/com/muzik/seodash/SeodashApplication.java
@@ -0,0 +1,13 @@
+package com.muzik.seodash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeodashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeodashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muzik/seodash/SeodashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muzik.seodash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeodashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
